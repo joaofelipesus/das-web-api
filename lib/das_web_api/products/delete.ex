7d1d29710caba57c2dbe593266855100b8bdf22a
Joao Felipe <joaofelipe1294@gmail.com>
2021-11-09 15:09:28 -0300

    fix: removed extra param.

--- lib/das_web_api/products/delete.ex
+++ lib/das_web_api/products/delete.ex
@@ -3,7 +3,7 @@ defmodule DasWebApi.Products.Delete do
   alias DasWebApi.Repo
   alias DasWebApi.Product
 
-  def call(%{"id" => id, "description" => description}) do
+  def call(%{"id" => id}) do
     product = Repo.get(Product, id)
     case product do
       nil -> nil
