3976dc8a0457af62b7ecde139da936ee63bdd284
joao.sus <joao.sus@deliverycenter.com>
2021-11-17 17:40:38 -0300

    feat: add employee model.

---
+++ lib/das_web_api/employee.ex
@@ -0,0 +1,21 @@
+defmodule DasWebApi.Employee do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :age]
+
+  schema "employees" do
+    field :name, :string
+    field :age, :integer
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @required_params)
+    |> validate_required([:name, :age])
+    |> unique_constraint([:name])
+  end
+end
