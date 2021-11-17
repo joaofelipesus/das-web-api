ae1e7379fc4a8d324c589450434163799593b32a
joao.sus <joao.sus@deliverycenter.com>
2021-11-17 17:54:37 -0300

    feat: add route show.

--- lib/das_web_api_web/controllers/employees_controller.ex
+++ lib/das_web_api_web/controllers/employees_controller.ex
@@ -9,4 +9,12 @@ defmodule DasWebApiWeb.EmployeesController do
     |> put_status(:ok)
     |> render("index.json", employees: Repo.all(Employee))
   end
+
+  def show(conn, %{"id" => id}) do
+    with %Employee{} = employee <- Repo.get(Employee, id) do
+      conn
+      |> put_status(:ok)
+      |> render("employee.json", employee: employee)
+    end
+  end
 end
--- lib/das_web_api_web/views/employees_view.ex
+++ lib/das_web_api_web/views/employees_view.ex
@@ -2,6 +2,12 @@ defmodule DasWebApiWeb.EmployeesView do
   use DasWebApiWeb, :view
 
   def render("index.json", %{employees: employees}) do
-    %{employees: employees}
+    %{employees: Enum.map(employees, fn e -> render_employee(e) end)}
+  end
+
+  def render("employee.json", %{employee: employee}), do: render_employee(employee)
+
+  defp render_employee(employee) do
+    %{employee: employee}
   end
 end
